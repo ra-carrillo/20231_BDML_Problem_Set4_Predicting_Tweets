afe6c8e09033d8e8c2ec0eb9cd3838c622f994c9
Andres Carrillo <racarrillo.research@gmail.com>
2023-03-20 12:37:11 -0500

    Update script1_Problem_Set4_data cleaning and statistics.R

--- scripts/script1_Problem_Set4_data cleaning and statistics.R
+++ scripts/script1_Problem_Set4_data cleaning and statistics.R
@@ -33,3 +33,37 @@ require("pacman")
 p_load(tm, # procesar,limpiar y analizar datos de texto
        tidyverse,stringi, tidytext, stopwords, wordcloud2, udpipe,
        ggcorrplot)
+
+#--- Establecer el directorio de la carpeta de datos
+
+setwd("C:/Users/andre/OneDrive/Github/Repositorios/20231_BDML_Problem_Set4_Predicting_Tweets/data/raw")
+
+
+#--- Importar base de datos : train y test
+
+  raw_train <- read.csv2("train.csv", 
+                     sep = ",", 
+                     dec = ".", 
+                     fileEncoding = "UTF-8")
+  
+  raw_test <- read.csv2("test.csv", 
+                        sep = ",", 
+                        dec = ".", 
+                        fileEncoding = "UTF-8")
+
+#--- Visualizar las dimensiones de cada matriz de datos [train-test]
+
+  dim(raw_train)  # Filas=9349 x Columnas=3
+  dim(raw_test)   # Filas=1500 x Columnas=2
+  
+  
+#--- Visualizar matriz de datos (En una pestaña) [db: raw_train]
+  
+  View(raw_train)
+  
+# La data:raw_train contiene tres variables: 
+#  1) id: identificador del tweet
+#  2) name: nombre del autor del tweet
+#  3) text: contenido textual del tweet
+  
+  
